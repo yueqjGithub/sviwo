fd3d2bdc5c37b57fde8d93a21124af04cc568f4a
yueqj <410281036@qq.com>
2024-05-19 23:16:30 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import { useEffect } from 'react'
+import './app.scss'
+import BackGround from './components/background'
+import SkyBg from './components/sky'
+function App() {
+  const setHtmlFontSize = (designWidth: number) => {
+    const html = document.getElementsByTagName('html')[0]
+    html.style.fontSize = `${(document.body.clientWidth / designWidth) * 625}%`
+  }
+  useEffect(() => {
+    setHtmlFontSize(1920)
+    window.addEventListener('resize', () => setHtmlFontSize(1920))
+    return () => {
+      window.removeEventListener('resize', () => setHtmlFontSize(1920))
+    }
+  }, [])
+  return (
+    <div className={`app-container`}>
+      <BackGround />
+      <SkyBg></SkyBg>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/background/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import styles from './index.module.scss'
+const BackGround:FC = () => {
+  return (
+    <div className={`${styles.container}`}></div>
+  )
+}
+
+
+export default BackGround
